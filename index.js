2652f91c70df3f049cd1fb0618c5dcb77bb67018
gutengboi <animashaunolayinka195@gmail.com>
2023-06-08 20:28:28 +0100

    first push

---
+++ index.js
@@ -0,0 +1,35 @@
+// imports from packages
+const express = require("express");
+const mongoose = require('mongoose'); 
+//import from other files
+const  authRouter = require('./routes/auth');
+const adminRouter = require("./routes/admin");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+
+
+//init
+const PORT = process.env.PORT || 3000;
+const app = express();
+const DB = 
+"mongodb+srv://anikala:Olayinka1@cluster0.rmmgsk5.mongodb.net/?retryWrites=true&w=majority";
+
+//middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+//connecttons
+mongoose
+.connect(DB)
+.then(() => {
+    console.log("connection Successful");
+})
+.catch((e) => {
+    console.log(e);
+});
+app.listen(PORT, "0.0.0.0",() => {
+    console.log('connected at port ${PORT}');
+});
